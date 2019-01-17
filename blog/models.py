74802ad0005c6a89cf0c3e3b5d504eafa24c5305
Debanik <debanik@live.com>
2019-01-17 22:26:53 +0530

    Complete website done

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,11 @@ class Blog(models.Model):
     pub_date = models.DateField(auto_now_add=True)
     body = models.TextField(max_length=1000)
     image = models.ImageField(upload_to = 'images/')
+
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100]
--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.allblogs, name = 'allblogs'),
+    path('<int:blog_id>/',views.details,name='individual_blog'),
 ]
